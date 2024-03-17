39862262e3576ffc5498b50768a04ff3af9e2a25
Albin <albifinn100@student.kau.se>
2024-03-17 14:49:20 +0100

    fixed errors

--- js/JSFunctions.js
+++ js/JSFunctions.js
@@ -1,293 +1,258 @@
 "use strict";
-//Run onload
-window.addEventListener("load", function (e) {
-    oGameData.initGlobalObject();
-    //Hides #game-area
-    document.querySelector("#game-area").classList.add("d-none");
-    //Adds click-listener to #newGame
-    document.querySelector("#newGame").addEventListener("click", function (e) {
-        validateForm();
-    });
+let oGameData = {};
+let REF_TABLE = document.querySelector("table");
+let REF_H1 = document.querySelector("div.jumbotron>h1");
+let REF_TDS = document.querySelectorAll("td");
+let REF_STARTBTN = document.querySelector("#newGame");
+let REF_ERRORMSG = document.querySelector("#errorMsg");
+let REF_FORM = document.querySelector("form");
+let REF_GAMEAREA = document.querySelector("#game-area");
+let REF_NICK1 = document.querySelector("#nick1");
+let REF_NICK2 = document.querySelector("#nick2");
+let REF_COLOR1 = document.querySelector("#color1");
+let REF_COLOR2 = document.querySelector("#color2");
+const BLACK = "#000000";
+const WHITE = "#ffffff";
+const IN_PROGRESS = 0;
+const WIN_X = 1;
+const WIN_O = 2;
+const DRAW = 3;
+const NAME_MAXLENGTH = 5;
+
+window.addEventListener("load", () => {
+  oGameData.initGlobalObject();
+  //Hides game area
+  REF_GAMEAREA.classList.add("d-none");
+
+  //Set function to btn onclick event
+  REF_STARTBTN.addEventListener("click", validateForm);
 });
 
-/**
- * Globalt objekt som innehåller de attribut som ni skall använda.
- * Initieras genom anrop till funktionern initGlobalObject().
- */
-let oGameData = {};
 
-const M_NO = "";
-const M_P1 = "X";
-const M_P2 = "O";
-
-/**
- * Initerar det globala objektet med de attribut som ni skall använda er av.
- * Funktionen tar inte emot några värden.
- * Funktionen returnerar inte något värde.
- */
 oGameData.initGlobalObject = function () {
 
-    //Datastruktur för vilka platser som är lediga respektive har brickor
-    oGameData.gameField = new Array(M_NO, M_NO, M_NO, M_NO, M_NO, M_NO, M_NO, M_NO, M_NO);
+  //Datastruktur för vilka platser som är lediga respektive har brickor
+  oGameData.gameField = Array('', '', '', '', '', '', '', '', '');
 
-    //Indikerar tecknet som skall användas för spelare ett.
-    oGameData.playerOne = M_P1;
+  //Indikerar tecknet som skall användas för spelare ett.
+  oGameData.playerOne = "X";
 
-    //Indikerar tecknet som skall användas för spelare två.
-    oGameData.playerTwo = M_P2;
+  //Indikerar tecknet som skall användas för spelare två.
+  oGameData.playerTwo = "O";
 
-    //Kan anta värdet X eller O och indikerar vilken spelare som för tillfället skall lägga sin "bricka".
-    oGameData.currentPlayer = "";
+  //Kan anta värdet X eller O och indikerar vilken spelare som för tillfället skall lägga sin "bricka".
+  oGameData.currentPlayer = "";
 
-    //Nickname för spelare ett som tilldelas från ett formulärelement,
-    oGameData.nickNamePlayerOne = "";
+  //Nickname för spelare ett som tilldelas från ett formulärelement,
+  oGameData.nickNamePlayerOne = "";
 
-    //Nickname för spelare två som tilldelas från ett formulärelement.
-    oGameData.nickNamePlayerTwo = "";
+  //Nickname för spelare två som tilldelas från ett formulärelement.
+  oGameData.nickNamePlayerTwo = "";
 
-    //Färg för spelare ett som tilldelas från ett formulärelement.
-    oGameData.colorPlayerOne = "";
+  //Färg för spelare ett som tilldelas från ett formulärelement.
+  oGameData.colorPlayerOne = "";
 
-    //Färg för spelare två som tilldelas från ett formulärelement.
-    oGameData.colorPlayerTwo = "";
+  //Färg för spelare två som tilldelas från ett formulärelement.
+  oGameData.colorPlayerTwo = "";
 
-    //"Flagga" som indikerar om användaren klickat för checkboken.
-    oGameData.timerEnabled = false;
+  //"Flagga" som indikerar om användaren klickat för checkboken.
+  oGameData.timerEnabled = false;
 
-    //Timerid om användaren har klickat för checkboxen. 
-    oGameData.timerId = null;
+  //Timerid om användaren har klickat för checkboxen. 
+  oGameData.timerId = null;
 
 }
-//Check if form-inputs are vaild
+
+
+oGameData.checkForGameOver = function () {
+  let p1Win = false;
+  let p2Win = false;
+
+  let player = [oGameData.playerOne, oGameData.playerTwo];
+
+  function checkFieldFull() {
+    for (let i = 0; i < oGameData.gameField.length; i++) {
+      if (oGameData.gameField[i] === "") {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  //Check each row: [0,1,2]; [3,4,5]; [6,7,8]
+  function checkHorizontal() {
+    for (let j = 0; j < player.length; j++) {
+      for (let i = 0; i <= 6; i += 3) {
+        if (oGameData.gameField[i] === player[j] && oGameData.gameField[i + 1] === player[j] && oGameData.gameField[i + 2] === player[j]) {
+          if (oGameData.gameField[i] === oGameData.playerOne) p1Win = true;
+          else p2Win = true;
+        }
+      }
+    }
+  }
+
+  //Check each column: [0,3,6]; [1,4,7]; [2,5,8]
+  function checkVertical() {
+    for (let j = 0; j < player.length; j++) {
+      for (let i = 0; i < 3; i++) {
+        if (oGameData.gameField[i] === player[j] && oGameData.gameField[i + 3] === player[j] && oGameData.gameField[i + 6] === player[j]) {
+          if (oGameData.gameField[i] === oGameData.playerOne) p1Win = true;
+          else p2Win = true;
+        }
+      }
+    }
+  }
+
+  //Check diagonals: [0,4,8]; [2,4,6]
+  function checkDiagonal() {
+    let arr = oGameData.gameField;
+    for (let i = 0; i < player.length; i++) {
+      if (arr[0] === player[i] && arr[4] === player[i] && arr[8] === player[i]) {
+        if (arr[0] == oGameData.playerOne) p1Win = true;
+        else p2Win = true;
+      }
+      if (arr[2] === player[i] && arr[4] === player[i] && arr[6] === player[i]) {
+        if (arr[2] == oGameData.playerOne) p1Win = true;
+        else p2Win = true;
+      }
+    }
+  }
+
+  let checks = [checkHorizontal(), checkVertical(), checkDiagonal()];
+  for (let i = 0; i < 3; i++) {
+    checks[i];
+  }
+  if (p1Win && !p2Win) return WIN_X;
+  else if (!p1Win && p2Win) return WIN_O;
+  else if (checkFieldFull()) return DRAW;
+  return IN_PROGRESS;
+
+
+}
+
+
 function validateForm() {
-    //Error-function for try-catch-block
-    //Also displays error-text in #errorMsg
-    function throwError(msg) {
-        document.querySelector("#errorMsg").textContent = msg;
-        throw new Error(msg);
-    }
-    //Constants
-    const NAME_MAX_LENGTH = 5;
-    const BLACK = "#000000";
-    const WHITE = "#ffffff";
+  try {
+    //Check if user inputs are correct
+    let name1 = REF_NICK1.value;
+    let name2 = REF_NICK2.value;
+    let color1 = REF_COLOR1.value;
+    let color2 = REF_COLOR2.value;
+    if (name1.length < NAME_MAXLENGTH || name2.length < NAME_MAXLENGTH) { throw ("Name too short"); }
+    if (name1 === name2) { throw ("Same names"); }
+    if (color1 === BLACK || color1 === WHITE) { throw ("Player 1: Invaild color"); }
+    if (color2 === BLACK || color2 === WHITE) { throw ("Player 2: Invaild color"); }
+    if (color1 === color2) { throw ("Same color"); }
 
-    try {
-        // Check name-length for both players
-        // Check if names are same
-        let player_names = document.querySelectorAll("input[placeholder='nickname']");
-        for (let name of player_names) {
-            if (name.value.length < NAME_MAX_LENGTH) {
-                throwError("A player name is too short!");
-            }
-        }
-        if (player_names[0].value == player_names[1].value) {
-            throwError("Both players can not have the same name!");
-        }
-
-        // Check color for both players
-        // Error if: both colors are the same or color is black or white
-        let player_colors = [
-            document.querySelector("#color1").value,
-            document.querySelector("#color2").value,
-        ];
-        for (let color of player_colors) {
-            if (color == WHITE || color == BLACK) {
-                throwError("A player has an invalid color!");
-            }
-        }
-        if (player_colors[0] == player_colors[1]) {
-            throwError("Both players have the same color!");
-        }
-    } catch (e) {
-        console.log(e);
-        return;
-    }
-    //Initiate game if no errors caught
+    //Start game if no errors
+    
     initiateGame();
+
+  }
+  catch (error) {
+    console.log("Hej");
+    REF_ERRORMSG.textContent = error;
+  }
 }
 
 
 function initiateGame() {
-    //Hides form
-    //Displays game-area
-    //Empites errormsg text
-    document.querySelector("form").classList.add("d-none");
-    document.querySelector("#game-area").classList.remove("d-none");
-    document.querySelector("#errorMsg").textContent = "";
+  
+  //Hide form and error msg, show game area
+  REF_FORM.classList.add("d-none");
+  REF_GAMEAREA.classList.remove("d-none");
+  REF_ERRORMSG.textContent = "";
 
-    //Read player-names
-    oGameData.nickNamePlayerOne = document.querySelector("#nick1").value;
-    oGameData.nickNamePlayerTwo = document.querySelector("#nick2").value;
+  //Player 1
+  oGameData.nickNamePlayerOne = REF_NICK1.value;
+  oGameData.colorPlayerOne = REF_COLOR1.value;
 
-    //Read player-colors
-    oGameData.colorPlayerOne = document.querySelector("#color1").value;
-    oGameData.colorPlayerTwo = document.querySelector("#color2").value;
+  //Player 2
+  oGameData.nickNamePlayerTwo = REF_NICK2.value;
+  oGameData.colorPlayerTwo = REF_COLOR2.value;
 
-    //Reset all table-cells in game-area
-    document.querySelectorAll("td").forEach((field) => {
-        field.textContent = "";
-        field.style.backgroundColor = "white";
-    });
+  //Reset table cells
+  for (let td of REF_TDS) {
+    td.textContent = "";
+    td.style.backgroundColor = "WHITE";
+  }
 
-    //Choose starting player randomly
-    let r = Math.random();
-    let playerChar, playerName;
+  //Set starting player
+  let playerChar = "";
+  let playerName = "";
+  if (Math.random() < 0.5) {
+    playerChar = oGameData.playerOne;
+    playerName = oGameData.nickNamePlayerOne;
+    oGameData.currentPlayer = oGameData.playerOne;
+  }
+  else {
+    playerChar = oGameData.playerTwo;
+    playerName = oGameData.nickNamePlayerTwo;
+    oGameData.currentPlayer = oGameData.playerTwo;
+  }
 
-    if (r < 0.5) {
-        oGameData.currentPlayer = playerChar = oGameData.playerOne;
-        playerName = oGameData.nickNamePlayerOne;
-    } else {
-        oGameData.currentPlayer = playerChar = oGameData.playerTwo;
-        playerName = oGameData.nickNamePlayerTwo;
-    }
-
-    //Display current player in h1
-    document.querySelector(".jumbotron h1").textContent = "Aktuell spelare är " + playerName;
+  REF_TABLE.addEventListener("click", executeMove);
 }
 
-/**
- * Kontrollerar för tre i rad.
- * Returnerar 0 om det inte är någon vinnare, 
- * returnerar 1 om spelaren med ett kryss (X) är vinnare,
- * returnerar 2 om spelaren med en cirkel (O) är vinnare eller
- * returnerar 3 om det är oavgjort.
- * Funktionen tar inte emot några värden.
- */
 
-oGameData.checkForGameOver = function () {
+//Runs when a move is executed by the current player
+//Fills table-cell with current players symbol and color if not occupied
+//Runs gameOverProtocol if win or draw
+function executeMove(event) {
+  let targetCell = event.target;
+  let targetId = targetCell.getAttribute("data-id");
 
-    const NO_WINNER = 0;
-    const X_WINNER = 1;
-    const O_WINNER = 2;
-    const DRAW = 3;
+  if (oGameData.gameField[targetId] === "") {
+    oGameData.gameField[targetId] = oGameData.currentPlayer;
+    targetCell.textContent = oGameData.currentPlayer;
 
-    let checkHorizontal = function () {
-        //Check 1st row
-        if (oGameData.gameField[0] == M_P1 && oGameData.gameField[1] == M_P1 && oGameData.gameField[2] == M_P1) {
-            return X_WINNER;
-        }
-        if (oGameData.gameField[0] == M_P2 && oGameData.gameField[1] == M_P2 && oGameData.gameField[2] == M_P2) {
-            return O_WINNER;
-        }
-
-        //Check 2nd row
-        if (oGameData.gameField[3] == M_P1 && oGameData.gameField[4] == M_P1 && oGameData.gameField[5] == M_P1) {
-            return X_WINNER;
-        }
-        if (oGameData.gameField[3] == M_P2 && oGameData.gameField[4] == M_P2 && oGameData.gameField[5] == M_P2) {
-            return O_WINNER;
-        }
-
-        //Check 3rd row
-        if (oGameData.gameField[6] == M_P1 && oGameData.gameField[7] == M_P1 && oGameData.gameField[8] == M_P1) {
-            return X_WINNER;
-        }
-        if (oGameData.gameField[6] == M_P2 && oGameData.gameField[7] == M_P2 && oGameData.gameField[8] == M_P2) {
-            return O_WINNER;
-        }
-        // No horizontal result
-        return NO_WINNER;
+    if (oGameData.currentPlayer === oGameData.playerOne) {
+      targetCell.style.backgroundColor = oGameData.colorPlayerOne;
+      setCurrentPlayer(oGameData.playerTwo, oGameData.nickNamePlayerTwo)
     }
-
-    let checkVertical = function () {
-        if (oGameData.gameField[0] === M_P1 && oGameData.gameField[3] === M_P1 && oGameData.gameField[6] === M_P1
-            || oGameData.gameField[1] === M_P1 && oGameData.gameField[4] === M_P1 && oGameData.gameField[7] === M_P1
-            || oGameData.gameField[2] === M_P1 && oGameData.gameField[5] === M_P1 && oGameData.gameField[8] === M_P1) {
-            return X_WINNER;
-        }
-        if (oGameData.gameField[0] === M_P2 && oGameData.gameField[3] === M_P2 && oGameData.gameField[6] === M_P2
-            || oGameData.gameField[1] === M_P2 && oGameData.gameField[4] === M_P2 && oGameData.gameField[7] === M_P2
-            || oGameData.gameField[2] === M_P2 && oGameData.gameField[5] === M_P2 && oGameData.gameField[8] === M_P2) {
-            return O_WINNER;
-        }
-        return NO_WINNER;
+    else {
+      targetCell.style.backgroundColor = oGameData.colorPlayerTwo;
+      setCurrentPlayer(oGameData.playerOne, oGameData.nickNamePlayerOne);
     }
-
-    let checkDiagonal = function () {
-        // Backslash Diagonal
-        if (oGameData.gameField[0] == M_P1 && oGameData.gameField[4] == M_P1 && oGameData.gameField[8] == M_P1) {
-            return X_WINNER;
-        }
-        if (oGameData.gameField[0] == M_P2 && oGameData.gameField[4] == M_P2 && oGameData.gameField[8] == M_P2) {
-            return O_WINNER;
-        }
-        // Forwardslash Diagonal
-        if (oGameData.gameField[2] == M_P1 && oGameData.gameField[4] == M_P1 && oGameData.gameField[6] == M_P1) {
-            return X_WINNER;
-        }
-        if (oGameData.gameField[2] == M_P2 && oGameData.gameField[4] == M_P2 && oGameData.gameField[6] == M_P2) {
-            return O_WINNER;
-        }
-        // No diagonal result
-        return NO_WINNER;
+    if (oGameData.checkForGameOver() !==IN_PROGRESS) {
+      gameOverProtocol();
     }
-
-    let checkArr = [checkHorizontal, checkVertical, checkDiagonal];
-    for (let i = 0; i < checkArr.length; i++) {
-        let result = checkArr[i]();
-        if (result !== NO_WINNER) {
-            return result;
-        }
-    }
-
-    // Check for draw
-    function isGameDrawn() {
-        for (let field of oGameData.gameField) {
-            if (field === M_NO) {
-                return false;
-            }
-        };
-        return true;
-    };
-    if (isGameDrawn()) {
-        return DRAW;
-    }
-
-    // There are still empty fields and no winner.
-    return NO_WINNER;
+  }
 }
 
-// Randomized Test
-function randomizedTest(amount) {
-    if (!Number.isInteger(amount) || amount < 1) {
-        amount = 1;
-    }
-
-    let possibleFields = [M_NO, M_P1, M_P2];
-
-    for (let i = 0; i < amount; i++) {
-
-        for (let i = 0; i < oGameData.gameField.length; i++) {
-            let setField = Math.floor(Math.random() * possibleFields.length);
-            oGameData.gameField[i] = possibleFields[setField];
-        };
-
-        function getField(field) {
-            if (field == "") {
-                return "_";
-            }
-            return field;
-        }
-
-        console.log("",
-            getField(oGameData.gameField[0]), getField(oGameData.gameField[1]), getField(oGameData.gameField[2]), "\n",
-            getField(oGameData.gameField[3]), getField(oGameData.gameField[4]), getField(oGameData.gameField[5]), "\n",
-            getField(oGameData.gameField[6]), getField(oGameData.gameField[7]), getField(oGameData.gameField[8]), "\n");
-
-        let result = oGameData.checkForGameOver();
-        switch (result) {
-            case NO_WINNER:
-                console.log("Game in progress...");
-                break;
-            case X_WINNER:
-                console.log("X won the game!");
-                break;
-            case O_WINNER:
-                console.log("O won the game!");
-                break;
-            case DRAW:
-                console.log("Game ended with a DRAW");
-                break;
-        }
-    }
+//Sets current player in GameData
+//Also updates h1
+function setCurrentPlayer(player, playerName) {
+  oGameData.currentPlayer = player;
+  REF_H1.textContent = "Aktuell spelare är: " + oGameData.currentPlayer + " (" + playerName + ")";
 }
+
+
+//Runs if game is over
+//Turns of game and hides gameField
+//Shows gameresult in h1
+//Resets GameData
+function gameOverProtocol() {
+  REF_TABLE.removeEventListener("click", executeMove);
+  REF_FORM.classList.remove("d-none");
+  let winnerName, winnerMark;
+  if (oGameData.checkForGameOver() === DRAW) REF_H1.textContent = "Oavgjort";
+  else {
+    switch (oGameData.checkForGameOver()) {
+      case WIN_X:
+        winnerName = oGameData.nickNamePlayerOne;
+        winnerMark = oGameData.playerOne;
+        break;
+      case WIN_O:
+        winnerName = oGameData.nickNamePlayerTwo;
+        winnerMark = oGameData.playerTwo;
+        break;
+    }
+    REF_H1.textContent = "Vinnare är: " + winnerName + " (" + winnerMark + ")" + " Spela igen?";
+  }
+
+  REF_GAMEAREA.classList.add("d-none");
+  oGameData.initGlobalObject();
+}
+
+
